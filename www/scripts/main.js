a378363f7b1d5f1988ee95957806934107206569
c3lphie <simon_sc@live.dk>
2019-03-12 22:31:06 +0100

    add table row working

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,3 +1,34 @@
-let eventTable = document.getElementById('eventList');
-let userDeviceTable = document.getElementById('userDeviceList');
-let deviceTable = document.getElementById('deviceList');
+// Get request function
+
+
+// Refresh side
+function refresh() {
+  location.reload(true);
+}
+
+//--------------------
+//    Tabel Id'er:
+// - eventList
+// - userDeviceList
+// - deviceList
+//--------------------
+
+// Ny tabelrække
+function newTableRow(tableId, cell1Text, cell2Text, cell3Text) {
+  var table = document.getElementById(tableId);
+  var row = table.insertRow(1);
+
+  if (cell3Text == null) {
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = cell1Text;
+    cell2.innerHTML = cell2Text;
+  } else {
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = cell1Text;
+    cell2.innerHTML = cell2Text;
+    cell3.innerHTML = cell3Text;
+  }
+}
