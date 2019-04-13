a3746e7ad19832aecc8551e979f34e0623cbce2e
c3lphie <simon_sc@live.dk>
2019-04-13 18:00:15 +0200

    test

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -12,8 +12,8 @@ function EventInit(){
 // Initialisering af index
 function HubInit(){
   GetDevices('http://10.0.0.1/api/getdevices.php');
-  GetEvents('http://127.0.0.1/api/getevents.php');
-  GetUserdevice('http://127.0.0.1/api/getuserdevices.php');
+  //GetEvents('http://127.0.0.1/api/getevents.php');
+  //GetUserdevice('http://127.0.0.1/api/getuserdevices.php');
 }
 
 
