f3ce2bb9ee0b2690b6a44b4d4b603097d35d3d41
c3lphie <simon_sc@live.dk>
2019-04-13 18:12:36 +0200

    nyt fix

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -3,6 +3,8 @@ function refresh() {
   location.reload(true);
 }
 
+document.addEventListener('DOMContentLoaded', HubInit, false);
+
 // Initialisering af ny event siden
 function EventInit(){
   AddDeviceToSelect('http://127.0.0.1/api/getdevices.php','devicelist');
@@ -14,6 +16,7 @@ function HubInit(){
   GetDevices('http://10.0.0.1/api/getdevices.php');
   GetEvents('http://127.0.0.1/api/getevents.php');
   GetUserdevice('http://127.0.0.1/api/getuserdevices.php');
+  console.log("ok");
 }
 
 
