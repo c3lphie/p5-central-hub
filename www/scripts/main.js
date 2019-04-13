2636fad4a3d6d14655659a4a45b768d9ebc8b9b1
c3lphie <simon_sc@live.dk>
2019-04-13 17:23:22 +0200

    newevent

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -11,8 +11,8 @@ function EventInit(){
 
 function HubInit(){
   GetDevices('http://127.0.0.1/api/getdevices.php');
-  GetEvents('http://127.0.0.1/api/getdevices.php');
-  GetUserdevice('http://127.0.0.1/api/getdevices.php');
+  GetEvents('http://127.0.0.1/api/getevents.php');
+  GetUserdevice('http://127.0.0.1/api/getuserdevices.php');
 }
 
 
