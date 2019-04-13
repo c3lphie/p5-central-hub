706533d1ea2564d1e85f10086cc2e46d487fe7d7
c3lphie <simon_sc@live.dk>
2019-04-13 19:52:57 +0200

    virkkkkk

--- www/scripts/event.js
+++ www/scripts/event.js
@@ -2,7 +2,7 @@ let devicelist;
 let eventname;
 let eventdesc;
 let udevicelist;
-let addEvent = document.getElementById("addevent"); 
+let addEvent;
 
 document.addEventListener('DOMContentLoaded', EventInit, false);
 
@@ -10,17 +10,17 @@ document.addEventListener('DOMContentLoaded', EventInit, false);
 function EventInit(){
     AddDeviceToSelect('http://10.0.0.1/api/getdevices.php','devicelist');
     AddDeviceToSelect('http://10.0.0.1/api/getuserdevices.php','udevicelist');
+}
+
+document.onload = function(){
     devicelist = document.getElementById("devicelist");
-eventname = document.getElementById("eventname");
-eventdesc = document.getElementById("eventdesc");
-udevicelist = document.getElementById("udevicelist");
-   
-  }
+    eventname = document.getElementById("eventname");
+    eventdesc = document.getElementById("eventdesc");
+    udevicelist = document.getElementById("udevicelist");
+    addEvent = document.getElementById("addevent"); 
 
+    addEvent.onclick = function(){
+        NewEvent(eventname.innerText, eventdesc.innerText, devicelist[devicelist.selectedIndex].value, udevicelist[udevicelist.selectedIndex].value)
+    }
 
-
- 
-
-addEvent.onclick = function(){
-    NewEvent(eventname.innerText, eventdesc.innerText, devicelist[devicelist.selectedIndex].value, udevicelist[udevicelist.selectedIndex].value)
 }
