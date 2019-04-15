c929e944f68867d7d4abbabb50921fb96389e4c7
c3lphie <simon_sc@live.dk>
2019-04-15 21:41:44 +0200

    aD

--- lib/Database.php
+++ lib/Database.php
@@ -145,7 +145,7 @@ class Database
     {
         $statement = $this->_conn->prepare("SELECT 1 FROM Targets WHERE Mac=? LIMIT 1");
 
-        $statement->bind_param("s", $mac);
+        $statement->bind_param("i", $mac);
 
         if (!$statement->execute()) Error("DeviceExists failed");
 
