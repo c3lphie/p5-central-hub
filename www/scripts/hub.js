bce23929f89fe59358fb7a05b83231b966142a08
c3lphie <simon_sc@live.dk>
2019-04-15 15:12:47 +0200

    asd

--- www/scripts/hub.js
+++ www/scripts/hub.js
@@ -5,7 +5,7 @@ function HubInit(){
   setInterval(() => {
     JSONToTable('http://10.0.0.1/api/getdevices.php','eventList');
     JSONToTable('http://10.0.0.1/api/getevents.php','deviceList');
-    JSONToTable('http://10.0.0.1/api/getuserdevices.php','userDeviceList');
+    JSONToTable('http://10.0.0.1/api/gettargets.php','userDeviceList');
   }, 5000); 
     
 }
