36edb1098ab5b013a2143a41a07acca75fee9313
c3lphie <simon_sc@live.dk>
2019-04-15 15:11:59 +0200

    event.js update

--- www/scripts/event.js
+++ www/scripts/event.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', EventInit, false);
 // Initialisering af ny event siden
 function EventInit(){
     AddDeviceToSelect('http://10.0.0.1/api/getdevices.php','devicelist');
-    AddDeviceToSelect('http://10.0.0.1/api/getuserdevices.php','udevicelist');
+    AddDeviceToSelect('http://10.0.0.1/api/gettargets.php','udevicelist');
     devicelist = document.getElementById("devicelist");
     eventname = document.getElementById("eventname");
     eventdesc = document.getElementById("eventdesc");
