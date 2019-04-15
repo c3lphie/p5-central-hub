8f1fe9a04e21ee1e5c47dfaa14b5b7f000c928ff
c3lphie <simon_sc@live.dk>
2019-04-15 15:14:37 +0200

    adnfkl

--- www/scripts/hub.js
+++ www/scripts/hub.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', HubInit, false);
 
 // Initialisering af index
 function HubInit(){
-  setInterval(() => {
-    JSONToTable('http://10.0.0.1/api/getdevices.php','eventList');
-    JSONToTable('http://10.0.0.1/api/getevents.php','deviceList');
-    JSONToTable('http://10.0.0.1/api/gettargets.php','userDeviceList');
-  }, 5000); 
+  // setInterval(() => {
+  //   JSONToTable('http://10.0.0.1/api/getdevices.php','eventList');
+  //   JSONToTable('http://10.0.0.1/api/getevents.php','deviceList');
+  //   JSONToTable('http://10.0.0.1/api/gettargets.php','userDeviceList');
+  // }, 5000); 
+  JSONToTable('http://10.0.0.1/api/getdevices.php','eventList');
+  JSONToTable('http://10.0.0.1/api/getevents.php','deviceList');
+  JSONToTable('http://10.0.0.1/api/gettargets.php','userDeviceList');
     
 }
