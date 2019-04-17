28b1843c72684146d708181947a3bba2ffb62331
c3lphie <simon_sc@live.dk>
2019-04-17 15:30:12 +0200

    fixed update 7913

--- lib/Database.php
+++ lib/Database.php
@@ -47,7 +47,8 @@ class Database
     {
         $statement = $this->_conn->prepare("INSERT INTO Devices (Mac, Ip, Type, LastSeen, Name) VALUES (?, ?, ?, ?, ?)");
 
-        if ($statement == false) Error("Could not create statement");
+        if ($statement == false) Error($statement->error);
+
 
         $statement->bind_param("ssiss", $device->GetMac(), $device->GetIp(), $device->GetType(), $device->GetLastSeen()->format("Y-m-d H:i:s"), $device->GetName());
 
